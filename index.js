49409c13e6589f191b03650b1f8e86bc47d2c044
Abisena <abisena27@gmail.com>
2025-11-06 23:47:42 +0700

    Commit (21): fix deploy vercel 3

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 import express from "express";
 import serverless from "serverless-http";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", path.join(process.cwd(), "views"));
-app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
+app.get("/", (req, res) => res.render("index.ejs"));
+app.get("/level1", (req, res) => res.render("level1.ejs"));
 
-app.get("/level1", (req, res) => {
-  res.render("level1.ejs");
-});
-
-// ❗ Jangan panggil app.listen()
-// Ekspor handler untuk Vercel
-export default serverless(app);
+export const handler = serverless(app);
+export default handler;
